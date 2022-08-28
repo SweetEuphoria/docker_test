e3e7d22bef15664084562d2f9d6d505de0ff7383
Mariya Shitova <64850298+SweetEuphoria@users.noreply.github.com>
2022-08-28 19:50:30 +0300

    Update init.sh

--- script/init.sh
+++ script/init.sh
@@ -4,4 +4,4 @@ airflow upgradedb
 sleep 5
 
 # Запуск шедулера и вебсервера
-airflow webserver & airflow scheduler
+airflow webserver & airflow scheduler & airflow create_user --email airflowadmin@example.com --firstname admin --lastname admin --password admin --role Admin --username admin

ff9a0d6d9cc4d6ef250c7f5982b7b3c76d5e5c0f
Mariya Shitova <64850298+SweetEuphoria@users.noreply.github.com>
2022-08-28 15:06:52 +0300

    Create create_user.sh

---
+++ script/create_user.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+airflow users create \
+          --username admin \
+          --firstname admin \
+          --lastname admin \
+          --password admin \
+          --role Admin \
+          --email admin@admin.com
